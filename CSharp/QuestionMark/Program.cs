fe0da01db244fde009492eeea0e458fe34984594
hieud16 <120547161+hieud16@users.noreply.github.com>
2022-12-14 13:34:21 +0700

    git commit -m"sd"

---
+++ CSharp/QuestionMark/Program.cs
@@ -0,0 +1,9 @@
+﻿//FormatException code ctrl+k+d
+double a = 0.1;
+double b = 0.2;
+Console.WriteLine(a + b == 0.3 ? "true" : "false");
+
+
+decimal c = 0.1M;
+decimal d = 0.2M;
+Console.WriteLine(c + d == 0.3M ? "true" : "false");
